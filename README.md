b8c89552e572c05d56501c79d10c976fb436533f
Akash Mane <akashmane277@gmail.com>
2023-09-27 15:19:09 +0530

    upload to  git

---
+++ README.md
@@ -0,0 +1,3 @@
+# Social Media App
+
+Developed a dynamic social media application using the MERN (MongoDB, Express.js, React, Node.js) stack, featuring user authentication, posting capabilities, friend management, likes, comments, and user and friend profile viewing.
---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,47 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+        >
+          Foxgram
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius={"1.5rem"}
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+          fontWeight="500"
+          variant="h5"
+          sx={{ m: "1.5rem" }}
+        >
+          Welcome to Foxgram, the social Media for Fox!
+        </Typography>
+        <Form />
+      </Box>
+
+    </Box>
+
+  )
+}
+
+export default LoginPage;
